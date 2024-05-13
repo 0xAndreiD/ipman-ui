f9a03651f3de94e633ac5edef3b78eb8d22d7ea0
0xAndreiD <andrei.dagami.me@gmail.com>
2024-05-14 06:38:31 +0900

    Add: common components

---
+++ components/Common/index.ts
@@ -0,0 +1,4 @@
+export { default as Container } from "./Container";
+export { default as FlexBox } from "./FlexBox";
+export { default as Form } from "./Form";
+export { default as FormControl } from "./FormControl";
