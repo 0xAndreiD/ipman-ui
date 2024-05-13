930010097e29f687fab5509d1fe1e64128d61d04
0xAndreiD <andrei.dagami.me@gmail.com>
2024-05-14 06:32:50 +0900

    Add: message contents

---
+++ enums/Message.ts
@@ -0,0 +1,9 @@
+enum Message {
+  UserRegistrationSuccess = "The user has successfully registerd.",
+  Ipv4RecordCreateSuccess = "The record has successfully created.",
+  Ipv4RecordUpdateSuccess = "The record has successfully updated.",
+  SomeInvalidFields = "Some fields are invalid.",
+  SomethingWrong = "Something went wrong.",
+}
+
+export default Message;
